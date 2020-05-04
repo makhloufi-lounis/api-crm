44faea56c4057b1ca01917af80d629559555909e
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-05-05 01:51:02 +0200

    setting up and configuring jwt authentication

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={
+ *          "groups"={"users_read"}
+ *     }
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity("email")
  */
@@ -22,13 +26,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"customers_read", "invoices_read", "invoices_subresource"})
+     * @Groups({"users_read", "customers_read", "invoices_read", "invoices_subresource"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresource"})
+     * @Groups({"users_read", "customers_read", "invoices_read", "invoices_subresource"})
      * @Assert\NotBlank(message="L'email doit être renseigné !")
      * @Assert\Email(message="L'adresse email doit avoir un format valide")
      */
@@ -36,7 +40,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="json")
-     * @Groups({"customers_read", "invoices_read"})
+     * @Groups({"users_read", "customers_read", "invoices_read"})
      */
     private $roles = [];
 
@@ -49,22 +53,22 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresource"})
+     * @Groups({"users_read", "customers_read", "invoices_read", "invoices_subresource"})
      * @Assert\NotBlank(message="Le prénom est obligatoire")
      * @Assert\Length(
      *     min=3, minMessage="Le prénom doit faire entre 3 et 255 caractères",
-     *     max=3, maxMessage="Le prénom doit faire entre 3 et 255 caractères"
+     *     max=255, maxMessage="Le prénom doit faire entre 3 et 255 caractères"
      * )
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresource"})
+     * @Groups({"users_read", "customers_read", "invoices_read", "invoices_subresource"})
      * @Assert\NotBlank(message="Le nom est obligatoire")
      * @Assert\Length(
      *     min=3, minMessage="Le nom doit faire entre 3 et 255 caractères",
-     *     max=3, maxMessage="Le nom doit faire entre 3 et 255 caractères"
+     *     max=255, maxMessage="Le nom doit faire entre 3 et 255 caractères"
      * )
      */
     private $lastName;
